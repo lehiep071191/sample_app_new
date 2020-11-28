051ad55ae088cf7c048944f421167fccb0f0c08f
lehiep071191 <good.boy.0991@gmail.com>
2020-11-28 21:28:59 +0700

    reset_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 	before_save :downcase_email
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :reset_token
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
@@ -32,6 +32,17 @@ class User < ApplicationRecord
 	end
 	def current_user?(user)
 		user && user == self
+	end
+	def create_reset_digest
+		self.reset_token = User.new_token
+		update_attribute(:reset_digest, User.digest(reset_token))
+		update_attribute(:reset_sent_at, Time.zone.now)
+	end
+	def send_password_reset_email
+		UserMailer.password_reset(self).deliver_now
+	end
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago
 	end
 	private 
 	def downcase_email
