8e59c78293d49567d02f41da3c7e1948a189fc9d
lehiep071191 <good.boy.0991@gmail.com>
2020-11-29 10:18:10 +0700

    fixxong

--- db/migrate/20201129015520_create_microposts.rb
+++ db/migrate/20201129015520_create_microposts.rb
@@ -1,7 +1,7 @@
 class CreateMicroposts < ActiveRecord::Migration[6.0]
   def change
     create_table :microposts do |t|
-      t.string :user_id, foreign_key: true
+      t.integer :user_id, foreign_key: true
       t.text :content
 
       t.timestamps
